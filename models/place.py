f10433045727ccf6bcf44f7d585f17c6e0953d9d
minions02 <adeneyo4jchrist@gmail.com>
2022-11-27 22:18:37 +0100

    hack

--- models/place.py
+++ models/place.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from models import amenity
+"""module place"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
+    """place class"""
     city_id = ""
     user_id = ""
     name = ""
